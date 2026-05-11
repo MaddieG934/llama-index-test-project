9dbd58c8d37aa54644b9eb4fb216bc697b080d21
MaddieG934 <madelynwesthuyzen@gmail.com>
2026-05-11 13:05:12 -0700

    Simple chat AI app using chainlit integrated with llama index, add data folder

--- app.py
+++ app.py
@@ -21,26 +21,68 @@ from llama_index.core.service_context import ServiceContext
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+# Load context
+try:
+    storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    index = load_index_from_storage(storage_context)
+except:
+    documents = SimpleDirectoryReader('./data').load_data(show_progress=True)
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist()
+
+# Start chat session
+@cl.on_chat_start
+async def start():
+    # Get model & define parameters
+    Settings.llm = OpenAI(
+        model='gpt-3.5-turbo', temperature=0.1, max_tokens=1024, streaming=True
+    )
+    Settings.embed_model = OpenAIEmbedding(model='text-embedding-3-small')
+    Settings.context_window = 4096
+
+    # 
+    service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
+    query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
+    cl.user_session.set('query_engine', query_engine)
+
+    # First prompt
+    await cl.Message(
+        author='Assistant', content='How may I help you?'
+    ).send()
+
+# Manage queries
+@cl.on_message
+async def main(message: cl.Message):
+    query_engine = cl.user_session.get('query_engine') # Instantiate RetrieverQueryEngine
+    
+    # Get prompt and generate response
+    msg = cl.Message(content='', author='Assistant')
+    res = await cl.make_async(query_engine.query)(message.content)
+
+    for token in res.response_gen:
+        await msg.stream_token(token)
+    await msg.send()
+
 # Define a tool for multiplying two numbers
-def multiply(a: float, b: float) -> float:
-    return a * b
+#def multiply(a: float, b: float) -> float:
+#    return a * b
 
 # Create an agent workflow with tools
-agent = FunctionAgent(
-    tools = [],
-    llm = OpenAI(model="gpt-4o-mini"),
-    system_prompt = "You are a helpful assistant that can multiply two numbers.",
-)
+#agent = FunctionAgent(
+#    tools = [],
+#    llm = OpenAI(model="gpt-4o-mini"),
+#    system_prompt = "You are a helpful assistant that can multiply two numbers.",
+#)
 
-async def main():
-    while True:
-        msg = input('How can I help you? Type E to exit: ')
+#async def main():
+#    while True:
+#        msg = input('How can I help you? Type E to exit: ')
 
-        if msg == 'E' or msg == 'e':
-            exit(0)
+#        if msg == 'E' or msg == 'e':
+#            exit(0)
         
-        response = await agent.run(user_msg = msg)
-        print(str(response))
+#        response = await agent.run(user_msg = msg)
+#        print(str(response))
 
-if __name__ == '__main__':
-    asyncio.run(main())
+#if __name__ == '__main__':
+#    asyncio.run(main())
