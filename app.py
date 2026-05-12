0a853755aee6f2f055043e24836dffbb577b6245
MaddieG934 <madelynwesthuyzen@gmail.com>
2026-05-12 10:18:59 -0700

    First attempt to incorporate function agent with tools

--- app.py
+++ app.py
@@ -33,14 +33,19 @@ except:
 # Start chat session
 @cl.on_chat_start
 async def start():
-    # Get model & define parameters
-    Settings.llm = OpenAI(
-        model='gpt-3.5-turbo', temperature=0.1, max_tokens=1024, streaming=True
+
+    # Create an agent workflow with tools
+    agent = FunctionAgent(
+        tools = [multiply],
+        llm = OpenAI(model='gpt-4o-mini', temperature=0.1, max_tokens=1024, streaming=True),
+        system_prompt = 'You are a helpful assistant that can multiply two numbers.',
     )
+
+    # Set model, embeddings, and context window
+    Settings.llm = agent.llm
     Settings.embed_model = OpenAIEmbedding(model='text-embedding-3-small')
     Settings.context_window = 4096
 
-    # 
     service_context = ServiceContext.from_defaults(callback_manager=CallbackManager([cl.LlamaIndexCallbackHandler()]))
     query_engine = index.as_query_engine(streaming=True, similarity_top_k=2, service_context=service_context)
     cl.user_session.set('query_engine', query_engine)
@@ -53,6 +58,7 @@ async def start():
 # Manage queries
 @cl.on_message
 async def main(message: cl.Message):
+
     query_engine = cl.user_session.get('query_engine') # Instantiate RetrieverQueryEngine
     
     # Get prompt and generate response
@@ -64,25 +70,5 @@ async def main(message: cl.Message):
     await msg.send()
 
 # Define a tool for multiplying two numbers
-#def multiply(a: float, b: float) -> float:
-#    return a * b
-
-# Create an agent workflow with tools
-#agent = FunctionAgent(
-#    tools = [],
-#    llm = OpenAI(model="gpt-4o-mini"),
-#    system_prompt = "You are a helpful assistant that can multiply two numbers.",
-#)
-
-#async def main():
-#    while True:
-#        msg = input('How can I help you? Type E to exit: ')
-
-#        if msg == 'E' or msg == 'e':
-#            exit(0)
-        
-#        response = await agent.run(user_msg = msg)
-#        print(str(response))
-
-#if __name__ == '__main__':
-#    asyncio.run(main())
+def multiply(a: float, b: float) -> float:
+    return a * b
